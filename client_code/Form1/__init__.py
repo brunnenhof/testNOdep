7b6e905d9a92219bd4df02c030021dee8bcb17ad
grippe@blue-way.net <grippe@blue-way.net>
2025-07-23 11:54:16 +0000

    Created LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,3 +7,25 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    self.label_2.text = 0
+    self.label_4.text = 0
+    self.label_6.text = 0
+
+  def press_me_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    nbr = 3
+    gives = self.label_6.text
+    org = self.label_2.text
+    new_nbr = org - nbr
+    self.label_6.text = new_nbr
+    self.label_4.text = nbr
+    self.label_2.text = new_nbr
+
+  def down_click(self, **event_args):
+    nbr =5
+    gives = self.label_6.text
+    org = self.label_2.text
+    new_nbr = org + nbr
+    self.label_6.text = new_nbr
+    self.label_4.text = nbr
+    self.label_2.text = new_nbr
